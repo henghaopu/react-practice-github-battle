30ce188da0bd3dca81c25bc156d775444e359817
Heng-Hao Pu <hpu@oswego.edu>
2020-01-10 23:22:34 -0500

    Enable css in index.js
    - npm install --save-dev css-loader style-loader
    - Configure both of them in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js"
   },
+  module: {
+    rules: [
+      // As every .css file is being imported/loaded, 
+      // the loader preprocesses/transforms the source code of the module.
+      //   css-loader: @import url(style.css) => require('./style.css')
+      //   style-loader: import "./xxx.css" => <style>...</style> 
+      {
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
   mode: "development"
 };
