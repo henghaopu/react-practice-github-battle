14fcde9be778b76b8fb26120ef4b43eff75ac4e3
Heng-Hao Pu <hpu@oswego.edu>
2020-01-16 22:59:53 -0500

    Style the Instructions component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -3,20 +3,20 @@ import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
 
 function Instructions() {
   return (
-    <div>
-      <h1>Instructions</h1>
-      <ol>
+    <div className="instructions-container">
+      <h1 className="center-text header-lg">Instructions</h1>
+      <ol className="container-sm grid center-text battle-instructions">
         <li>
-          <h3>Enter two GitHub users</h3>
-          <FaUserFriends/>
+          <h3 className="header-sm">Enter two GitHub users</h3>
+          <FaUserFriends className="bg-light" color="rgb(255, 191, 116)" size={140}/>
         </li>
         <li>
-          <h3>Battle</h3>
-          <FaFighterJet/>
+          <h3 className="header-sm">Battle</h3>
+          <FaFighterJet className="bg-light" color="#727272" size={140}/>
         </li>
         <li>
-          <h3>See the winner</h3>
-          <FaTrophy/>
+          <h3 className="header-sm">See the winner</h3>
+          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140}/>
         </li>
       </ol>
     </div>
