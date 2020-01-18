2c59cb0c18f495edb0bea063d8b8ad165972cd24
Heng-Hao Pu <hpu@oswego.edu>
2020-01-17 23:57:04 -0500

    Style RepoCards stateless functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { fetchPopularRepos } from "../utils/api";
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from "react-icons/fa";
 
 function Nav({ selected, onUpdateTopic }) {
   const topics = ["JavaScript", "React", "Vue", "Angular", "Ember", "Polymer"];
@@ -29,8 +30,50 @@ Nav.propTypes = {
 
 function RepoCards({ repos }) {
   return (
-    <ul>
-      <pre>{JSON.stringify(repos, null, 2)}</pre>
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const {
+          name,
+          owner,
+          html_url,
+          stargazers_count,
+          forks,
+          open_issues
+        } = repo;
+        const { login, avatar_url } = owner;
+
+        return (
+          <li key={html_url} className="repo-bg-light">
+            <h4 className="header-lg center-text">#{index + 1}</h4>
+            <img className="avatar" src={avatar_url} alt={`Avatar for ${login}`}/>
+            <h2 className="center-text">
+              <a className="link" href={html_url}>{login}</a>
+            </h2>
+            <ul className="card-list">
+              <li>
+                <FaUser color="rgb(255, 191, 116)" size={22}/>
+                <a href={`https://github.com/${login}`}>
+                  {login}
+                </a>
+              </li>
+              <li>
+                <FaStar color="rgb(255, 215, 0)" size={22}/>
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color="rgb(129, 195, 245)" size={22}/>
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22}/>
+                {open_issues.toLocaleString()} open issues
+              </li>
+            </ul>
+            
+          </li>
+        )
+
+      })}
     </ul>
   );
 }
