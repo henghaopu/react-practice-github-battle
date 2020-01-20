3161e11ad62691b74b55ff1918584c6fb0673228
Heng-Hao Pu <hpu@oswego.edu>
2020-01-19 22:47:21 -0500

    Create a controlled component
    - Handle the logic flow of a form with a label, an input, and a button

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import PropTypes from "prop-types";
 
 function Instructions() {
   return (
@@ -8,26 +9,82 @@ function Instructions() {
       <ol className="container-sm grid center-text battle-instructions">
         <li>
           <h3 className="header-sm">Enter two GitHub users</h3>
-          <FaUserFriends className="bg-light" color="rgb(255, 191, 116)" size={140}/>
+          <FaUserFriends
+            className="bg-light"
+            color="rgb(255, 191, 116)"
+            size={140}
+          />
         </li>
         <li>
           <h3 className="header-sm">Battle</h3>
-          <FaFighterJet className="bg-light" color="#727272" size={140}/>
+          <FaFighterJet className="bg-light" color="#727272" size={140} />
         </li>
         <li>
           <h3 className="header-sm">See the winner</h3>
-          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140}/>
+          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140} />
         </li>
       </ol>
     </div>
-    );
+  );
 }
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">{this.props.label}</label>
+        <div>
+          <input
+            type="text"
+            id="username"
+            placeholder="github username"
+            autoComplete="off"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button type="submit" disabled={!this.state.username}>
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
 
 export default class Battle extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Instructions />
+        <PlayerInput label="myname" onSubmit={value => console.log(value)} />
       </React.Fragment>
     );
   }
