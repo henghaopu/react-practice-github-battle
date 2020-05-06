8a113cb5236db04aeef626755aa4701b22af5b9a
Heng-Hao Pu <hpu@oswego.edu>
2020-05-06 19:48:16 -0400

    Set up webpack production mode

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
     // webpack-devserver: 404s will fallback to /index.html
-    publicPath: '/'
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -16,7 +16,7 @@ module.exports = {
       // and specify the transformations we want babel to make in package.json
       {
         test: /\.js$/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
       // As every .css file is being imported/loaded,
       // the loader preprocesses/transforms the source code of the module.
@@ -24,20 +24,20 @@ module.exports = {
       //   style-loader: import "./xxx.css" => <style>...</style>
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   // plugins allow webpack to execute certain tasks after the bundle
   // has been created.
   plugins: [
     new HtmlWebpackPlugin({
-      template: "app/index.html"
-    })
+      template: "app/index.html",
+    }),
   ],
-  // webpack-devserver: 404s will fallback 
+  // webpack-devserver: 404s will fallback
   devServer: {
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
